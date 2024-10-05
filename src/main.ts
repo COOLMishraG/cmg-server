821653ac99c6539c18ca251b45ed39a226eb7de9
COOLMishraG <anujmishra04@outlook.com>
2024-10-05 15:15:20 +0530

    #6

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  // Set the port to listen from the environment variable or default to 3000
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
